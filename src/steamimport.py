e68afa87e6f6e13676f4700064f758c8fb8d9171
Wastfulajr <austin_rhee@comcast.net>
2023-08-02 18:11:58 -0400

    Added abiliy to move data to CSV

---
+++ src/steamimport.py
@@ -0,0 +1,60 @@
+import csv
+import json
+import time
+import datetime
+import statistics
+import os
+
+import requests as rq  # Used to interface with Steam API HTTP/S requests
+
+def urlbase():
+    """Base API url for Steamspy"""
+    return "https://steamspy.com/api.php"
+
+def def_params():
+    """Params for API call"""
+    return {"request": "all", "page": "0"}
+
+def json_req(url=None, params=None):
+    """Given a url, return the JSON response."""
+    if url is None:
+        print("Error: no URL.\n")
+        return
+    try:
+        jsonReq = rq.get(url=url, params=params)  # Send a GET request to the Steam APi
+    except Exception as e:
+        print("Excpetion during GET Request: ", e)  # Prints err if rq.get() fails
+        print("\nRe-trying request in 5 seconds.")
+        time.sleep(5)
+        return json_req(url, params)  # Recurse, and attempt to re-make request
+    if jsonReq:  # If the request is successful, get the response json
+        return jsonReq.json()
+
+def dataRequest(page="0"):
+    """Given a page, call json_req to return that page. Default page = 0"""
+    params = def_params()
+    params["pages"] = page
+    data = json_req(urlbase(), def_params())
+    return data
+
+def printData(data):
+    """Given a dict, read out data"""
+    for key in data:
+        print("Key: ", key)
+        for info in data[key]:
+            print(info, ": ", data[key][info])
+        print("\n")
+
+def saveData(data):
+    """Given a dict of data, save to a csv file"""
+    with open('test/steamspy.csv', 'w', encoding="utf-8", newline='') as f:
+        data_fields = ['appid', 'name', 'developer', 'publisher', 
+                       'score_rank', 'positive', 'negative', 'userscore',
+                       'owners', 'average_forever', 'average_2weeks',
+                       'median_forever', 'median_2weeks', 'price',
+                       'initialprice', 'discount', 'ccu']
+        wr = csv.DictWriter(f, fieldnames=data_fields)
+        wr.writeheader()
+        for key in data:
+            wr.writerow(data[key])
+
