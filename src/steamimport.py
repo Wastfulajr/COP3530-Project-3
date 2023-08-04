69f92708a9b9103ecff6832f5e8baa28c6a76808
Wastfulajr <austin_rhee@comcast.net>
2023-08-04 13:42:52 -0400

    Initial support for Steam Official API

--- src/steamimport.py
+++ src/steamimport.py
@@ -39,8 +39,8 @@ def json_req(url=None, params=None):
 
 def dataRequest(url=urlbase(), params=def_params()):
     """Given a page, call json_req to return that page.
-       Default parameters defined by def_params()
-       Default url defined by urlbase()"""
+       Default url defined by urlbase().
+       Default parameters defined by def_params()."""
     data = json_req(url, params)
     if not data:
        print("Failed to process json request for", url)
@@ -59,12 +59,15 @@ def printData(data):
 
 def saveData(data, path='data/', file='steamspy.csv', data_fields=fieldsBase()):
     """Given a dict of data, save to a csv file.
-    Takes a path as an argument, default data/steamspy.csv"""
+       Takes path, file, datafields as args.
+       path: data/
+       file: steamspy.csv
+       data_fields: fieldsBase()"""
     dataExists = os.path.isfile(path)
     try:
         if not dataExists:  # Case where CSV does not exist, write header & enter 'write' mode
             print("Creating new file", file)
-            with open(path, 'w', encoding="utf-8", newline='') as f:
+            with open(path + file, 'w', encoding="utf-8", newline='') as f:
                 wr = csv.DictWriter(f, fieldnames=data_fields)  # Pass dict() to writehead
                 wr.writeheader()  # Pass all data_fields as header
                 for key in data:
@@ -72,10 +75,19 @@ def saveData(data, path='data/', file='steamspy.csv', data_fields=fieldsBase()):
                 print("Write complete!")
         else:  # Case where CSV does exist, do not write header & enter 'append' mode
             print("Appending to file", file)
-            with open(path, 'a', encoding="utf-8", newline='') as f:
+            with open(path + file, 'a', encoding="utf-8", newline='') as f:
                 wr = csv.DictWriter(f, fieldnames=data_fields)  # Pass dict() to writehead
                 for key in data:
                     wr.writerow(data[key])
                 print("Write complete!")
     except Exception as e:
         print("Writing failed! Exception:", e)
+
+def cleanSteamData(data):
+    data_clean = {}
+    for applist in data:  # Only iterates ONCE
+        for app in data[applist]:  # Only iterates ONCE
+            for game in data[applist][app]:
+                data_clean[game['appid']] = game
+    print("Cleaned Steam Data.")
+    return data_clean
